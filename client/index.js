e4f4abba1b33779547b03754ab1f92a070fa541a
Reland Boyle <reland.boyle@gmail.com>
2021-10-09 00:53:24 -0400

    After Midnight

--- client/index.js
+++ client/index.js
@@ -1,10 +1,52 @@
-
-const username = document.getElementById(loginUsername);
-
-function writeNote(username, password) {
-  console.log(username);
+function logMeIn() {
+  const username = document.querySelector('#loginUsername');
+  const password = document.querySelector('#loginPassword');
+  console.log([username.value, password.value]);
+  // if username matches a user in the db,
+  // if password matches the password on that user,
+  // display the user's notes
+  // else alert: "Invalid Username or Password"
 }
 
-function signUp(url) {
+
+function signMeUp() {
+  const loginScreen = document.querySelector('#loginScreen');
+  const createUserScreen = document.querySelector('#createUserScreen');
+  loginScreen.style.display = 'none';
+  createUserScreen.style.display = 'inline';
+}
+
+
+function createNewUser() {
+  const newUser = document.querySelector('#createUsername');
+  const newPassword = document.querySelector('#createPassword');
+  console.log([newUser.value, newPassword.value]);
+}
+
+
+function backToLogin() {
+  const loginScreen = document.querySelector('#loginScreen');
+  const createUserScreen = document.querySelector('#createUserScreen');
+  loginScreen.style.display = 'inline';
+  createUserScreen.style.display = 'none';
+}
+
+
+function createNewNote(title, content) {
+
+}
+
+
+function backToNotes() {
+
+}
+
+
+function newNoteScreen() {
+
+}
+
+
+function logMeOut() {
 
 }
